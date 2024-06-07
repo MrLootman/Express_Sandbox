4f7b473210f1ab21668c83aa9265ebbdea7a0eb0
MrLootman <113359328+MrLootman@users.noreply.github.com>
2024-06-07 23:31:16 +0200

    Update migrate.js

--- src/database/migrate.js
+++ src/database/migrate.js
@@ -11,7 +11,6 @@ const database = mysql.createPool({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
     multipleStatements: true
 });
 
